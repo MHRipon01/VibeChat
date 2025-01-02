8f1ab4f0effac99d3f76d105b57646613e385d29
MHRipon01 <mhripon012@gmail.com>
2025-01-01 19:28:13 -0800

    socket io issue solved

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -21,7 +21,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://vibechat-prod.onrender.com", {
         query: {
           userId: authUser._id,
         },
