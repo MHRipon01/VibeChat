5d1136c72a1c42dbd02b4acd683797a8516e47d1
MHRipon01 <mhripon012@gmail.com>
2025-01-01 18:38:04 -0800

    trying to fix

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import useConversation from "../../zustand/useConversation";
 // import MessageInput from "./MessageInput";
-import Messages from "/Messages";
+import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
 import { useAuthContext } from "../../context/AuthContext";
 import MessageInput from "./MessageInput";
